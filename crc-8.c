b69391a38476f4166c7b02729a75a6e993c151ea
r3v <r3v>
2023-12-16 13:51:51 -0800

    Will correctly compute a crc8 over a byte array of data and then shows how to verify that data say in the case of sending over the wire

--- crc-8.c
+++ crc-8.c
@@ -11,56 +11,56 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define BUF_SIZE 256
+
 /**
  * compute_crc8
  * 
  * Given data and a polynomial, will calcuate the crc-8 and return the result
  * 
- * @param data Data to be crc'd
+ * @param bytes Byte array of data to have crc calculated
+ * @param len   Number of bytes in bytes array
  * @param poly 9 bit crc-8 polynomial to compute calculation
  * 
  * @return the result of the calculation
 */
-uint8_t compute_crc8(uint8_t data, uint8_t poly) { 
-   uint8_t crc = data; // Crc register 
+uint8_t compute_crc8(uint8_t* bytes, int len, uint8_t poly) { 
+   uint8_t crc = 0;                   // Crc register 
 
-   for (int i = 0; i < 8; i++) { 
-      if ((crc & 0x80) != 0) { 
-         crc = (uint8_t)((crc << 1)^poly);
-      } else
-         crc = crc << 1;
-      printf("0x%x\n", crc);
+   for (int b = 0; b < len; b++) {
+      uint8_t data = (uint8_t)(bytes[b] ^ crc);
+
+      // Caclulate crc for this byte 
+      for (int i = 0; i < 8; i++) { 
+         if ((data & 0x80) != 0) { 
+            data = (uint8_t)((data << 1)^poly);
+         } else
+            data = data << 1;
+         crc = data;
+      }
    }
 
    return crc;
 
 }
 
-/**
- * crc8_verf 
- * 
- * Performs crc verification on data to check for any bit errors
- * 
- * @param data Data to perform crc verification over
- * @param result Crc-8 result to ensure there are not bit errors
- * 
- * @return Returns 1 if the data passes the crc, 0 otherwise
-*/
-int crc8_verf(uint8_t data, uint8_t result) { 
-   return 1;
-}
-
 int main() {
-   uint8_t in_data = 0xC2;      // Input data
-   uint16_t poly = 0b00011101; // CRC-8 polynomial
+   uint8_t in_data[BUF_SIZE] = {0xDE, 0xAD, 0xBE, 0xEF}; // Input data
+   int len = 4; // Get the length of byte array
+
+   uint8_t poly = 0x1D;    // CRC-8 generator polynomial
 
    // Calculate the crc computation 
-   printf("Calculate 0x%x with poly 0x%x...\n", in_data, poly);
-   uint8_t result = compute_crc8(in_data, poly);
+   printf("Calculate 0x%x with poly 0x%x...\n", in_data[0], poly);
+   uint8_t result = compute_crc8(in_data, len, poly);
 
-   printf("Result: 0x%x\n", result);
-   // Do crc verification
-   //printf("Performing CRC verification using 0x%x::STATUS::%s\n", result, crc8_verf(in_data, result) ? "PASSED" :"FAILED");
+   printf("Remained: 0x%x\n", result);
 
+   printf("Crc verification: ");
+   in_data[len] = result;
+   
+   uint8_t rem = compute_crc8(in_data, len+1, poly);
+   printf("%s --> 0x%x\n", rem ? "FAILED" : "PASSED", rem);
+   
 	return 0;   
 }
